dca468ce97b68fe9d1e4c5a08f4adff69640fe37
Mark <37282432+Mark-in-Motion@users.noreply.github.com>
2025-02-19 12:26:23 +0800

    used isresizable

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,11 +9,12 @@ export interface TextareaProps
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, isResizable, ...props }, ref) => {
+  ({ className, isResizable = true, ...props }, ref) => {
     return (
       <textarea
         className={cn(
           "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          !isResizable && "resize-none",
           className
         )}
         ref={ref}
